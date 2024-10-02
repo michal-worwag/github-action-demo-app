5ac081816accb0b17b4dcc89da762fe9e0cdf218
Michał Worwąg <michalwo@gmail.com>
2024-10-02 18:55:07 +0200

    add error in test

--- src/utils/counterTransformer.test.ts
+++ src/utils/counterTransformer.test.ts
@@ -11,6 +11,6 @@ describe('counterTransformer', () => {
   test('should decrease value', () => {
     expect(decrease(4)).toBe(3);
     expect(decrease(8)).toBe(6);
-    expect(decrease(21)).toBe(18);
+    expect(decrease(21)).toBe(44);
   });
 });
